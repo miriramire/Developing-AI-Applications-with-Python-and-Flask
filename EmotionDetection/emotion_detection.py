aaae0584248d956b828d41b4a6fa667b4caa6f18
miriramire <miriramire@gmail.com>
2026-05-06 09:31:53 -0500

    Refactor emotion detection logic and response handling
    Refactor emotion score extraction and dominant emotion calculation based on response status. Handle cases for successful and unsuccessful responses.

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,22 +7,41 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
-    emotions_list = [anger_score, disgust_score, fear_score, joy_score, sadness_score]
+
+    if response.status_code == 200:
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        emotions_list = [anger_score, disgust_score, fear_score, joy_score, sadness_score]
+        dominant_emotion = None
+        for k, v in result.items():
+            if v == max(emotions_list):
+                dominant_emotion = k
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        emotions_list = None
+        dominant_emotion = None
+    else:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        emotions_list = None
+        dominant_emotion = None
     result = {
         'anger': anger_score,
         'disgust': disgust_score,
         'fear': fear_score,
         'joy': joy_score,
         'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
     }
-    dominant_emotion = ''
-    for k, v in result.items():
-        if v == max(emotions_list):
-            dominant_emotion = k
-    result['dominant_emotion'] = dominant_emotion
+
     return result
